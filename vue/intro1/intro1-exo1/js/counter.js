c76a3fdb7d75299038371eb0b478612d9600ec07
Rafiklatrikdu69 <rafik.bouchenna050@email.com>
2024-02-05 08:53:50 +0100

    intro 1

---
+++ vue/intro1/intro1-exo1/js/counter.js
@@ -0,0 +1,27 @@
+export default {
+    props:["start"],
+    setup(props) {
+       
+        const count = Vue.ref(0);
+        let intervalId;
+        Vue.onMounted(()=>{
+            intervalId=setInterval(() => {
+                count.value += 1;
+            }, 1000);
+        });
+        Vue.onUnmounted(()=>clearInterval(intervalId));
+        function display(){ // méthode display
+            if(count.value< props.start){
+                return"En attente"
+            }
+            else{ 
+                let s = count.value%60;
+                let m = Math.floor(count.value/60);
+                return m + " minutes et " + s + " secondes" ;
+            }
+        }
+        // exposition pour le template de la méthode
+        return { display };
+    },
+    template:"<p>temps écoulé: {{display()}}</p>"
+}
--- vue/intro1/intro1-exo1/js/script.js
+++ vue/intro1/intro1-exo1/js/script.js
@@ -1,4 +1,17 @@
+// const App = Vue.createApp({
+//     template: "<p>Nouveau point de vueJs</p> <p>Quoicoubeh !</p>"
+//     });
+//     App.mount("#myApp");
+
+
+
+    // le composant est écrit dans counter.js
+import Counter from "./counter.js";
 const App = Vue.createApp({
-    template: "<p>Nouveau point de vueJs</p> <p>Quoicoubeh !</p>"
-    });
-    App.mount("#myApp");
+    // composants utilisables dans le template
+    components: {
+        Counter
+    },
+    template: '<Counter :start="5"/> ',
+});
+App.mount("#myApp");
